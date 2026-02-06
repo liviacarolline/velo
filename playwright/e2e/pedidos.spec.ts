954d588a82e534b12fc333d8d589ef95fe0a7676
Livia Caroline <livia.carolline.93@gmail.com>
2026-02-05 21:21:39 -0300

    adicionando abordagem com snapshot

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -68,12 +68,43 @@ test('deve consultar pedido aprovado', async ({ page }) => {
 
   //abordagem 3
 
-  const containerPedido = page.getByRole('paragraph')
-    .filter({ hasText: /^Pedido$/ })
-    .locator('..') //vai para o elemento pai
+  // const containerPedido = page.getByRole('paragraph')
+  //   .filter({ hasText: /^Pedido$/ })
+  //   .locator('..') //vai para o elemento pai
 
-  await expect(containerPedido).toContainText(order, { timeout: 10_000 });
-  await expect(page.getByText('APROVADO')).toBeVisible();
+  // await expect(containerPedido).toContainText(order, { timeout: 10_000 });
+  // await expect(page.getByText('APROVADO')).toBeVisible();
+
+  //abordagem 4
+
+  await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+    - img
+    - paragraph: Pedido
+    - paragraph: ${order}
+    - img
+    - text: APROVADO
+    - img "Velô Sprint"
+    - paragraph: Modelo
+    - paragraph: Velô Sprint
+    - paragraph: Cor
+    - paragraph: Midnight Black
+    - paragraph: Interior
+    - paragraph: cream
+    - paragraph: Rodas
+    - paragraph: sport Wheels
+    - heading "Dados do Cliente" [level=4]
+    - paragraph: Nome
+    - paragraph: Lívia Teste
+    - paragraph: Email
+    - paragraph: teste@teste.com
+    - paragraph: Loja de Retirada
+    - paragraph
+    - paragraph: Data do Pedido
+    - paragraph: /\\d+\\/\\d+\\/\\d+/
+    - heading "Pagamento" [level=4]
+    - paragraph: À Vista
+    - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
 
 });
 
